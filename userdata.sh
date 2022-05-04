129e4dbe10072dfcb3550b7b04321e9f87dd0b88
devsigi <52584911+devsigi@users.noreply.github.com>
2022-05-03 23:54:22 -0500

    Update userdata.sh
    fixed userdata as per aws linux commands

--- userdata.sh
+++ userdata.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 sudo yum -y update
+sudo yum upgrade
 # Install OpenJdk 8
-yum install java-11-openjdk-devel -y
+sudo amazon-linux-extras install java-openjdk11 -y
 # Install wget
 yum install wget -y
 # Install Jenkins
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum -y update
-yum install jenkins -y
-service jenkins start
+sudo yum install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl status jenkins
