59e3ac4e68287ab25949594dd7d3f6ad6f7d7e58
dev <devsigi@yahoo.com>
2022-05-04 11:04:48 -0500

    updated userdata for rhel box

--- userdata.sh
+++ userdata.sh
@@ -2,13 +2,13 @@
 sudo yum -y update
 sudo yum upgrade
 # Install OpenJdk 8
-sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install java-11-openjdk-devel -y
+#sudo amazon-linux-extras install java-openjdk11 -y
 # Install wget
-yum install wget -y
+sudo yum install wget -y
 # Install Jenkins
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum -y update
 sudo yum install jenkins -y
 sudo systemctl start jenkins
-sudo systemctl status jenkins
