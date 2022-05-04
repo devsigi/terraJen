ef9892dc1c39f4cd2c863be9bac898698d35762d
dev <devsigi@yahoo.com>
2022-05-03 23:49:01 -0500

    added userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sudo yum -y update
+sudo yum update
 # Install OpenJdk 8
-yum install java-11-openjdk-devel -y
+sudo amazon-linux-extras install java-openjdk11
 # Install wget
 yum install wget -y
 # Install Jenkins
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum -y update
-yum install jenkins -y
-service jenkins start
+sudo yum update
+sudo yum install jenkins -y
+sudo systemctl start jenkins
