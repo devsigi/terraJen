065477af663bfb355d2142c3f55ac4a836bb63a4
dev <devsigi@yahoo.com>
2022-05-04 11:11:46 -0500

    update export java var

--- userdata.sh
+++ userdata.sh
@@ -3,6 +3,8 @@ sudo yum -y update
 sudo yum upgrade
 # Install OpenJdk 8
 sudo yum install java-11-openjdk-devel -y
+export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-11.0.15.0.9-2.el8_5.x86_64"
+export PATH=$JAVA_HOME/bin:$PATH
 #sudo amazon-linux-extras install java-openjdk11 -y
 # Install wget
 sudo yum install wget -y
