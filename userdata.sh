41f1689126f3e04e41ecc571fa073868d467f0d4
dev <devsigi@yahoo.com>
2022-05-04 21:30:39 -0500

    moved export statements to end

--- userdata.sh
+++ userdata.sh
@@ -3,8 +3,6 @@ sudo yum -y update
 sudo yum upgrade
 # Install OpenJdk 8
 sudo yum install java-11-openjdk-devel -y
-export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-11.0.15.0.9-2.el8_5.x86_64"
-export PATH=$JAVA_HOME/bin:$PATH
 #sudo amazon-linux-extras install java-openjdk11 -y
 # Install wget
 sudo yum install wget -y
@@ -14,3 +12,5 @@ sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum -y update
 sudo yum install jenkins -y
 sudo systemctl start jenkins
+export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-11.0.15.0.9-2.el8_5.x86_64"
+export PATH=$JAVA_HOME/bin:$PATH
